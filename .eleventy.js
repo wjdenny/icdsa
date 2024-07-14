ecb068cffb583f7711f6b5c1caf06152695f169a
Will Denny <will@wjdenny.com>
2024-07-14 17:04:03 -0500

    fix!: copy over CNAME file to published branch

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function (eleventyConfig) {
     // Recursively copy over all files from src/assets to public/assets
     eleventyConfig.addPassthroughCopy("src/assets");
 
+    // Copy the CNAME file so our custom domain works
+    eleventyConfig.addPassthroughCopy("CNAME");
+
 	return {
         dir: {
             input:  "src",           // set the source directory from where Markdown and template files are read
